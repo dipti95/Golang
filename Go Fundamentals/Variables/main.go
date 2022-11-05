996026482957be314e5d6e960d825169c01cc36c
Ubuntu <azureuser@golang-vm-dipti.vw35djkbomku3dnwu4xv1wapba.bx.internal.cloudapp.net>
2022-11-05 23:10:28 +0000

    go variables

--- Go Fundamentals/Variables/main.go
+++ Go Fundamentals/Variables/main.go
@@ -13,4 +13,23 @@ func main() {
 	fmt.Println("Released year:", released)
 	fmt.Println("Is a programming language:",isAProgrammingLanguage)
 
+
+	// var product = "T-shirt"
+	// var cost = 20
+
+	// Inside the function we can also write variable shown below
+	product:= "T-shirt"
+	cost:= 20
+
+
+
+  fmt.Println("product's value is:", product)
+
+  fmt.Printf("product's type is: %T\n", product)
+  // we can change the value of cost because cost is not constant
+  cost = 15
+  fmt.Println("product's cost is:", cost)
+ 
+  fmt.Printf("cost type is: %T\n", cost)
+
 }
