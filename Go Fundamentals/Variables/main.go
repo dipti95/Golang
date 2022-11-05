cec59c2e04e347859925edae5a805c6a622fdf0d
Ubuntu <azureuser@golang-vm-dipti.vw35djkbomku3dnwu4xv1wapba.bx.internal.cloudapp.net>
2022-11-05 23:05:14 +0000

    variables exercise

---
+++ Go Fundamentals/Variables/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	
+	var language string = "Go"
+	const released int = 2009
+	var isAProgrammingLanguage = true
+   
+
+	fmt.Println("Language:",language)
+	fmt.Println("Released year:", released)
+	fmt.Println("Is a programming language:",isAProgrammingLanguage)
+
+}
