6b19768302267fbd5e82438a6ca27ceea46ac2b6
Ubuntu <azureuser@golang-vm-dipti.vw35djkbomku3dnwu4xv1wapba.bx.internal.cloudapp.net>
2022-11-06 20:59:14 +0000

    function

--- Go Fundamentals/Functions/main.go
+++ Go Fundamentals/Functions/main.go
@@ -18,9 +18,20 @@ func sayLoudly(phase string) string{
 	return strings.ToUpper(phase)
 }
 
+func getRectangleArea(width , length int)string{
+    product := width * length;
+
+	if product < 50 {
+		return fmt.Sprintf("The area is %d, which is less than 50", product)
+	}else {
+		return fmt.Sprintf("The area is %d, which is greater or equal to 50", product)
+	}
+}
+
 func main(){
    fmt.Println(add(4, 5));
    fmt.Println(sayHello("Dipti"))
    fmt.Println(sayLoudly("I am developer"))
+   fmt.Println(getRectangleArea(5,10))
    
 }
