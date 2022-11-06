b2a944091ed387df615122655a7627998c811b15
dipti95 <dipti.yadav@outlook.com>
2022-11-06 21:58:38 +0000

    Loops

---
+++ Go Fundamentals/Loops/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+
+func main()  {
+	
+//example1
+	for i:=0;i<=20;i++{
+		if i%2 == 0{
+		fmt.Println(i)
+		}
+	   
+	}
+// example2
+	for i:=0; i <=20; i+=2{
+		fmt.Println(i)
+	}
+// example3
+ i:=0
+for i <=20{
+	fmt.Println(i)
+	i++
+}
+
+
+}
+
