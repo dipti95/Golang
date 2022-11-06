8d90d8f03156af124759295f7f1a7d13db184b08
dipti95 <dipti.yadav@outlook.com>
2022-11-06 21:41:12 +0000

    Array and Slice example

---
+++ Go Fundamentals/Arrays and Slices/main.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+
+func main(){
+	
+	// var fibonacciArray [8] int
+
+	// fibonacciArray[0]=0;
+	// fibonacciArray[1]=1
+	// fibonacciArray[2]=1
+	// fibonacciArray[3]=2
+	// fibonacciArray[4]=3
+	// fibonacciArray[5]=5
+	// fibonacciArray[6]=8
+	// fibonacciArray[7]=13
+ 
+    /** Array :
+	   1. contains an ordered list of elements of single type
+	   2. contains a specific number of elements
+	*/     
+
+	// we can also write like this
+	 fibonacciArray := [8]int{0,1,1,2,3,5,8,13}
+
+	fmt.Println(fibonacciArray)
+	fmt.Println(fibonacciArray[0])
+	fmt.Println(fibonacciArray[7])
+	fmt.Println(len(fibonacciArray))
+	fmt.Println(fibonacciArray[0:4])
+
+	/** 
+	Slice:
+	1. Contains an ordered list of elements of single type
+	2. Contains a list of items that can expand and shrink.(This is the key difference between a slice and an array!)
+	*/
+
+	fruitSlice := []string{"Pear", "Cherry", "Durian"}
+
+	fmt.Println(fruitSlice)
+	fmt.Println(fruitSlice[2])
+	fmt.Println(len(fruitSlice))
+	fmt.Println(fruitSlice[0:2])
+
+	fruitSlice = append(fruitSlice,"Grape", "Mango" )
+     
+
+	fmt.Println(fruitSlice)
+	
+
+}
