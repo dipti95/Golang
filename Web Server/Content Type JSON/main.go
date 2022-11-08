85ea6281c9740cf4dedecf3ec5f51cb61ba96ab2
dipti95 <dipti.yadav@outlook.com>
2022-11-08 21:23:18 +0000

    Content type in JSON

---
+++ Web Server/Content Type JSON/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+
+var dictionary = map[string]string{
+	"Go" :"A programming language.",
+	"Gopher":"A software engineer who builds with Go.",
+	"Golang":"Another name for Go.",
+}
+
+func getDictionary(w http.ResponseWriter, r* http.Request ){
+    w.Header().Set("Content-Type", "application/json")
+
+	w.WriteHeader(http.StatusOK)
+
+	json.NewEncoder(w).Encode(dictionary)
+}
+
+
+func main(){
+
+   http.HandleFunc("/", getDictionary)
+
+	fmt.Println("Server Listening port 3000...")
+	http.ListenAndServe(":3000", nil)
+}
