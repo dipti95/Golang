cbeffcf992741acd6f9c22d961d58856b661f689
dipti95 <dipti.yadav@outlook.com>
2022-11-08 21:34:54 +0000

    exercise

--- Web Server/Content Type JSON/main.go
+++ Web Server/Content Type JSON/main.go
@@ -13,6 +13,15 @@ var dictionary = map[string]string{
 	"Golang":"Another name for Go.",
 }
 
+//Exercise
+var cityPopulations = map[string]uint32{
+	"Tokyo":       37435191,
+	"Delhi":       29399141,
+	"Shanghai":    26317104,
+	"Sao Paulo":   21846507,
+	"Mexico City": 21671908,
+}
+
 func getDictionary(w http.ResponseWriter, r* http.Request ){
     w.Header().Set("Content-Type", "application/json")
 
@@ -21,10 +30,21 @@ func getDictionary(w http.ResponseWriter, r* http.Request ){
 	json.NewEncoder(w).Encode(dictionary)
 }
 
+func index(w http.ResponseWriter, r* http.Request){
+	w.Header().Set("Content-Type", "HTML")
+
+	w.WriteHeader(http.StatusOK)
+
+	for key, val := range cityPopulations{
+		fmt.Fprintf(w, "<h2>%s: %d</h2>", key, val)
+	}
+}
+
 
 func main(){
 
    http.HandleFunc("/", getDictionary)
+   http.HandleFunc("/cities", index)
 
 	fmt.Println("Server Listening port 3000...")
 	http.ListenAndServe(":3000", nil)
