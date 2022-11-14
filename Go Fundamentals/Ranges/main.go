0734836eaead386ad59a0e4d9237fb7dfe2b8b66
dipti95 <dipti.yadav@outlook.com>
2022-11-14 20:18:52 +0000

    added comment

--- Go Fundamentals/Ranges/main.go
+++ Go Fundamentals/Ranges/main.go
@@ -3,21 +3,22 @@ package main
 import "fmt"
 
 func main() {
-   
-	athletes := []string{"Stephen", "Klay", "Harrison","Draymond", "Andrew"}
 
-	for i, name:= range athletes{
-		fmt.Printf("i = %d, name= %s\n",i, name)
+	athletes := []string{"Stephen", "Klay", "Harrison", "Draymond", "Andrew"}
+
+	// range is collection
+	for i, name := range athletes {
+		fmt.Printf("i = %d, name= %s\n", i, name)
 	}
 
 	for _, name := range athletes {
-       fmt.Printf("Name =%s\n", name)
+		fmt.Printf("Name =%s\n", name)
 	}
 
-	numbers := []int{30,11,40,23,12}
+	numbers := []int{30, 11, 40, 23, 12}
 
-	for _, num := range numbers{
-		if num %2 == 0 {
+	for _, num := range numbers {
+		if num%2 == 0 {
 			fmt.Println(num)
 		}
 	}
@@ -25,12 +26,11 @@ func main() {
 	fmt.Println(reduce([]int{0, 1, 1, 2, 3, 5, 8, 13, 21}))
 }
 
-
 func reduce(list []int) int {
 	sum := 0
 
-	for _, num := range list{
-		sum += num;
+	for _, num := range list {
+		sum += num
 
 	}
 
